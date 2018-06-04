ce355069ab8f5a7bbedd84b25abdaa16fd6249d0
Andrei Pimenov <andreipimenov@gmail.com>
2018-06-04 15:20:51 +0300

    Implemented set, get, unset and count

--- cmd/hashmap/main.go
+++ cmd/hashmap/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/andreipimenov/hashmap"
+)
+
+func main() {
+	//Create hash map
+	hm, _ := hashmap.NewHashMap(128)
+
+	//Set entries
+	hm.Set("Hello", "World")
+	hm.Set("Names", []string{"John", "Jane", "Melissa"})
+	hm.Set("Ages", map[string]float64{"John": 33, "Jane": 19})
+
+	//Get entries
+	v1, _ := hm.Get("Hello")
+	v2, _ := hm.Get("Names")
+	v3, _ := hm.Get("Ages")
+	fmt.Printf("%v->%v\n%v->%v\n%v->%v\n", "Hello", v1, "Names", v2, "Ages", v3)
+
+	//Get count of entries
+	fmt.Printf("Count of entries is %d\n", hm.Count())
+
+	//Unset entry
+	hm.Unset("Hello")
+
+	//Try to get removed entry
+	_, err := hm.Get("Hello")
+	if err != nil {
+		fmt.Printf("Trying to get removed entry with key Hello. Returned error: %v\n", err)
+	}
+
+	//Check count of entries
+	fmt.Printf("Count of entries now is %d\n", hm.Count())
 }
--- hashmap.go
+++ hashmap.go
@@ -1,7 +1,12 @@
 package hashmap
 
-// Key is of dynamic type
-type Key interface{}
+import (
+	"errors"
+	"fmt"
+)
+
+// Key is of type string
+type Key string
 
 // HashMaper interface describes hash map methods:
 // set pair of key->value, get value by key, unset pair by key, get count of key->value pairs
@@ -24,40 +29,113 @@ type HashFunc func(blockSize int, key Key) int
 
 // HashMap implements hash map data structure with defined block size, hash function and list of entries
 type HashMap struct {
-	BlockSize int
-	HashFunc  HashFunc
-	Entries   []*Entry
+	BlockSize    int
+	HashFunc     HashFunc
+	Entries      []*Entry
+	EntriesCount int
+}
+
+// hashFunc is default hash function
+func hashFunc(blockSize int, key Key) int {
+	h := 0
+	for i := 0; i < len(key); i++ {
+		h = 31*h + int(key[i])
+	}
+	return h % blockSize
 }
 
 // NewHashMap creates new hash map and returns pointer to it
-func NewHashMap(blockSize int, fn ...HashFunc) HashMaper {
-	hashFunc := HashFunc(nil)
+func NewHashMap(blockSize int, fn ...HashFunc) (HashMaper, error) {
+	if blockSize <= 0 {
+		return nil, errors.New("blockSize must be more than 0")
+	}
+	f := hashFunc
 	if len(fn) > 0 {
-		hashFunc = fn[0]
+		f = fn[0]
 	}
 	return &HashMap{
-		BlockSize: blockSize,
-		HashFunc:  hashFunc,
-		Entries:   make([]*Entry, blockSize),
-	}
+		BlockSize:    blockSize,
+		HashFunc:     f,
+		Entries:      make([]*Entry, blockSize),
+		EntriesCount: 0,
+	}, nil
 }
 
-// Set sets key->value entry
+// Set sets key->value entry: insert or update
 func (h *HashMap) Set(key Key, value interface{}) error {
+	i := h.HashFunc(h.BlockSize, key)
+	if i > len(h.Entries)-1 {
+		return fmt.Errorf("index %d is out of range", i)
+	}
+	entry := h.Entries[i]
+	if entry == nil {
+		h.Entries[i] = &Entry{
+			Key:   key,
+			Value: value,
+		}
+		h.EntriesCount++
+		return nil
+	}
+	for entry != nil {
+		if entry.Key == key {
+			entry.Value = value
+			return nil
+		}
+		if entry.NextEntry == nil {
+			entry.NextEntry = &Entry{
+				Key:   key,
+				Value: value,
+			}
+			h.EntriesCount++
+			return nil
+		}
+		entry = entry.NextEntry
+	}
 	return nil
 }
 
 // Get returns value by key or error otherwise
 func (h *HashMap) Get(key Key) (value interface{}, err error) {
-	return nil, nil
+	i := h.HashFunc(h.BlockSize, key)
+	if i > len(h.Entries)-1 {
+		return nil, fmt.Errorf("index %d is out of range", i)
+	}
+	entry := h.Entries[i]
+	for entry != nil {
+		if entry.Key == key {
+			return entry.Value, nil
+		}
+		entry = entry.NextEntry
+	}
+	return nil, fmt.Errorf("key %s not found", key)
 }
 
 // Unset removes entry by key and returns nil or error otherwise
 func (h *HashMap) Unset(key Key) error {
-	return nil
+	i := h.HashFunc(h.BlockSize, key)
+	if i > len(h.Entries)-1 {
+		return fmt.Errorf("index %d is out of range", i)
+	}
+	entry := h.Entries[i]
+	prevEntry := entry
+	for entry != nil {
+		if entry.Key == key {
+			if prevEntry == entry {
+				h.Entries[i] = entry.NextEntry
+			} else {
+				prevEntry.NextEntry = entry.NextEntry
+			}
+			entry = nil
+			h.EntriesCount--
+			return nil
+		}
+		prevEntry = entry
+		entry = entry.NextEntry
+	}
+	return fmt.Errorf("key %s not found", key)
 }
 
-// Count returns number of entries
+// Count returns count of entries
 func (h *HashMap) Count() int {
-	return 0
+	return h.EntriesCount
 }
